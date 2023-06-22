cbf57a53a33a583a95a44ff9a4ee1b0a7d538cb6
jpuljko <jxp5761@rit.edu>
2023-06-22 16:38:44 +0200

    Name and date manipulation with useState

--- src/EsignatureApp.jsx
+++ src/EsignatureApp.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import Title from "./components/Title";
 
 export default function EsignatureApp() {
+    const [name, setName] = useState("Your signature");
+    const [date, setDate] = useState("");
 
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    }
+    const handleDateChange = (e) => {
+        setDate(e.target.value);
+    }
     const inputStyle ={
         border: "none",
         borderBottom: '2px dotted',
@@ -12,8 +20,8 @@ export default function EsignatureApp() {
     document.body.style.background = "#eee";
     return (
         <div className='container text-center'>
-            <Title classes={"title"} text={"Name"} />
-            <Title classes={"main-title"} text={"Date"} />
+            <Title classes={"title"} text={name} />
+            <Title classes={"main-title"} text={!date ? "DoB" : date} />
             <p>
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             </p>
@@ -24,8 +32,13 @@ export default function EsignatureApp() {
                     top: "40vh"
                 }}
             >
-                <input type="date" value={""} style={inputStyle}></input>
-                <input type="text" value={""} style={inputStyle}></input>
+                <input type="date" value={date} style={inputStyle} onChange={handleDateChange} />
+                <input 
+                    type="text" 
+                    value={name} 
+                    style={inputStyle} 
+                    onChange={handleNameChange} 
+                />
             </footer>
             
         </div>
